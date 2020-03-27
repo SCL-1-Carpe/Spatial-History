4adb0535ef6e050708c7277ab3c35e06c981958a
TaikiTakami <takamitaiki0120@gmail.com>
2020-03-27 20:04:23 +0900

    リファクタリング

--- Assets/Networking/ExampleScripts/ObjectBasedTransformReplicator.cs
+++ Assets/Networking/ExampleScripts/ObjectBasedTransformReplicator.cs
@@ -6,7 +6,7 @@ public class ObjectBasedTransformReplicator : ReplicatiorBase
 {
     public GameObject CoordinateBaseObject;
 
-    Vector3 Prepos;
+    Vector3 prepos;
 
     private void Start()
     {
@@ -16,7 +16,6 @@ public class ObjectBasedTransformReplicator : ReplicatiorBase
         if (CoordinateBaseObject == null)
         {
             CoordinateBaseObject = GameObject.FindGameObjectWithTag("COBaseTag");
-
         }
 
 
@@ -26,12 +25,15 @@ public class ObjectBasedTransformReplicator : ReplicatiorBase
     {
         if (CoordinateBaseObject == null)
             return null;
-        Vector3 vec = (transform.position - CoordinateBaseObject.transform.position);
 
-        Vector3 Degradedvec = new Vector3(Vector3.Dot(vec, CoordinateBaseObject.transform.right), Vector3.Dot(vec, CoordinateBaseObject.transform.up), Vector3.Dot(vec, CoordinateBaseObject.transform.forward));
-        float yrot = Quaternion.LookRotation(transform.position, CoordinateBaseObject.transform.position).eulerAngles.y;
-        Prepos = transform.position;
-        return NetworkManagerBase.encoding.GetBytes(Serializer.Vector3ToString(Degradedvec, 3) + "," + yrot);
+        Vector3 vec = (transform.position - CoordinateBaseObject.transform.position);
+        float   x = Vector3.Dot(vec, CoordinateBaseObject.transform.right),
+                y = Vector3.Dot(vec, CoordinateBaseObject.transform.up),
+                z = Vector3.Dot(vec, CoordinateBaseObject.transform.forward);
+        Vector3 Degradedvec = new Vector3(x, y, z);
+        float yRot = Quaternion.LookRotation(transform.position, CoordinateBaseObject.transform.position).eulerAngles.y;
+        prepos = transform.position;
+        return NetworkManagerBase.encoding.GetBytes(Serializer.Vector3ToString(Degradedvec, 3) + "," + yRot);
 
     }
 
@@ -44,9 +46,13 @@ public class ObjectBasedTransformReplicator : ReplicatiorBase
     {
         if (CoordinateBaseObject == null)
             return;
+
         string[] s = NetworkManagerBase.encoding.GetString(repdata).Split(',');
         Vector3 vec = Serializer.StringToVector3(s[0], s[1], s[2]);
-        transform.position = CoordinateBaseObject.transform.position + CoordinateBaseObject.transform.right * vec.x + CoordinateBaseObject.transform.up * vec.y + CoordinateBaseObject.transform.forward * vec.z;
+        Vector3 diff_X = vec.x * CoordinateBaseObject.transform.right,
+                diff_Y = vec.y * CoordinateBaseObject.transform.up,
+                diff_Z = vec.z * CoordinateBaseObject.transform.forward;
+        transform.position = CoordinateBaseObject.transform.position + diff_X + diff_Y + diff_Z;
         transform.eulerAngles = new Vector3(0, float.Parse(s[3]), 0);
     }
 
@@ -57,11 +63,11 @@ public class ObjectBasedTransformReplicator : ReplicatiorBase
 
     public override bool DoesServerNeedReplication()
     {
-        return Prepos != transform.position&&IsAutonomousObject();
+        return prepos != transform.position&&IsAutonomousObject();
     }
 
     public override bool DoesClientNeedReplication(ClientDataContainer client)
     {
-        return Prepos != transform.position&&!IsAutonomousObject();
+        return prepos != transform.position&&!IsAutonomousObject();
     }
 }
